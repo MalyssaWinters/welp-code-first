fe85c9e9b88dad42ac6d73426a036755beb180b6
Malyssa Winters <malyssawinters5@gmail.com>
2016-07-25 16:05:56 -0400

    added context.cs

---
+++ WelpCodeFirst/Models/WelpCodeFirstContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WelpCodeFirst.Models
+{
+    public class WelpCodeFirstContext : DbContext
+    {
+        public DbSet<Business> Businesses { get; set; }
+        public DbSet<BusinessType> BusinessTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
